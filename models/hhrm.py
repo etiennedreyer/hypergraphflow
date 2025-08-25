4def0ec8395c9e9a4286ee551f99ecd33050a7f4
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2025-08-25 12:15:14 +0300

    hhrm overtraining very successfully. Using hyperedge rep for z_H and node rep for z_L with cross attention between for updates.

--- models/hhrm.py
+++ models/hhrm.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 import yaml
-from models.attention import SelfAttentionLayer
+from models.attention import SelfAttentionLayer, DecoderBlock
 from models.mlp import MLP
 from dataclasses import dataclass
+import math
 
 @dataclass
 class HiddenState:
@@ -49,46 +50,49 @@ class HHRM(nn.Module):
             activation=emb_cfg['activation']
         )
 
+        ### Edge positional embedding
+        self.edge_embedder = nn.Embedding(self.num_edges, self.hidden_dim)
+        
         ### Injection layer norm
         self.norm_L = nn.LayerNorm(self.hidden_dim, elementwise_affine=False)
         self.norm_H = nn.LayerNorm(self.hidden_dim, elementwise_affine=False)
 
-        ### Node self-attention (low-level)
-        SA_L_cfg = self.config['node_SA_L']
-        self.SA_L = nn.ModuleList([
-                            SelfAttentionLayer(
-                                model_dim=SA_L_cfg['model_dim'],
-                                num_heads=SA_L_cfg['num_heads'],
-                                activation=SA_L_cfg['activation'],
-                                gated=SA_L_cfg['gated'],
-                                scaling=SA_L_cfg['scaling'],
-                                ffn_factor=SA_L_cfg['ffn_factor'],
+        ### Nodes (low-level) updated based on hyperedges (high-level)
+        CA_L_cfg = self.config['node_CA_L']
+        self.CA_L = nn.ModuleList([
+                            DecoderBlock(
+                                model_dim=CA_L_cfg['model_dim'],
+                                num_heads=CA_L_cfg['num_heads'],
+                                activation=CA_L_cfg['activation'],
+                                gated=CA_L_cfg['gated'],
+                                scaling=CA_L_cfg['scaling'],
+                                ffn_factor=CA_L_cfg['ffn_factor'],
                             )
-                            for _ in range(SA_L_cfg['num_layers'])
+                            for _ in range(CA_L_cfg['num_layers'])
                         ])
         
-        ### Node self-attention (high-level)
-        SA_H_cfg = self.config['node_SA_H']
-        self.SA_H = nn.ModuleList([
-                            SelfAttentionLayer(
-                                model_dim=SA_H_cfg['model_dim'],
-                                num_heads=SA_H_cfg['num_heads'],
-                                activation=SA_H_cfg['activation'],
-                                gated=SA_H_cfg['gated'],
-                                scaling=SA_H_cfg['scaling'],
-                                ffn_factor=SA_H_cfg['ffn_factor'],
+        ### Hyperedges (high-level) updated based on nodes (low-level)
+        CA_H_cfg = self.config['edge_CA_H']
+        self.CA_H = nn.ModuleList([
+                            DecoderBlock(
+                                model_dim=CA_H_cfg['model_dim'],
+                                num_heads=CA_H_cfg['num_heads'],
+                                activation=CA_H_cfg['activation'],
+                                gated=CA_H_cfg['gated'],
+                                scaling=CA_H_cfg['scaling'],
+                                ffn_factor=CA_H_cfg['ffn_factor'],
                             )
-                            for _ in range(SA_H_cfg['num_layers'])
+                            for _ in range(CA_H_cfg['num_layers'])
                         ])
 
         ### Incidence predictor
-        inc_pred_cfg = self.config['incidence_predictor']
-        self.incidence_predictor = MLP(
-                    input_dim=inc_pred_cfg['input_dim'],
-                    layers=inc_pred_cfg['layers'],
-                    output_dim=inc_pred_cfg['output_dim'],
-                    activation=inc_pred_cfg['activation']
-        )
+        # inc_pred_cfg = self.config['incidence_predictor']
+        # self.incidence_predictor = MLP(
+        #             input_dim=inc_pred_cfg['input_dim'],
+        #             layers=inc_pred_cfg['layers'],
+        #             output_dim=inc_pred_cfg['output_dim'],
+        #             activation=inc_pred_cfg['activation']
+        # )
 
         ### Indicator predictor
         ind_pred_cfg = self.config['indicator_predictor']
@@ -118,6 +122,11 @@ class HHRM(nn.Module):
         if input_state.shape[-1] == self.num_node_features:
             input_state = self.node_embedder(input_state)
 
+        ### Hyperedge positional embedding
+        edge_pos_idx = torch.arange(self.num_edges, device=input_state.device).unsqueeze(0).expand(input_state.shape[0], -1)
+        edge_pos_emb = self.edge_embedder(edge_pos_idx)
+        z_H = z_H + edge_pos_emb
+
         ### Forward up to last iteration
         with torch.no_grad():
             for iter_H in range(self.iters_H):
@@ -128,32 +137,34 @@ class HHRM(nn.Module):
 
                     if not (last_iter_H and last_iter_L):
                         ### Low-level update
-                        z_L = self.norm_L(z_L + z_H + input_state)
-                        for SA in self.SA_L:
-                            z_L = SA(z_L, key_padding_mask=node_mask)
+                        z_L = self.norm_L(z_L + input_state)
+                        for block in self.CA_L:
+                            z_L = block(z_L, z_H, key_padding_mask_SA=node_mask)
 
                 if not last_iter_H:
                     ### High-level update
-                    z_H = self.norm_H(z_H + z_L)
-                    for SA in self.SA_H:
-                        z_H = SA(z_H, key_padding_mask=node_mask)
+                    z_H = self.norm_H(z_H + edge_pos_emb)
+                    for block in self.CA_H:
+                        z_H = block(z_H, z_L, key_padding_mask_CA=node_mask)
 
         assert not z_H.requires_grad and not z_L.requires_grad
 
         ### 1-step gradient approximation
-        z_L = self.norm_L(z_L + z_H + input_state)
-        for SA in self.SA_L:
-            z_L = SA(z_L, key_padding_mask=node_mask)
-        z_H = self.norm_H(z_H + z_L)
-        for SA in self.SA_H:
-            z_H = SA(z_H, key_padding_mask=node_mask)
+        z_L = self.norm_L(z_L + input_state)
+        for block in self.CA_L:
+            z_L = block(z_L, z_H, key_padding_mask_SA=node_mask)
+        z_H = self.norm_H(z_H + edge_pos_emb)
+        for block in self.CA_H:
+            z_H = block(z_H, z_L, key_padding_mask_CA=node_mask)
 
         ### prediction
-        inc = self.incidence_predictor(z_H) # (B, N, K)
-        ind = self.indicator_predictor(z_H.mean(dim=1, keepdim=True)) # (B, 1, K)
-        im = torch.cat([inc, ind], dim=1) # (B, N+1, K)
-        # im = torch.sigmoid(im)
-        im = im.transpose(1, 2)
+        # inc = self.incidence_predictor(z_H) # (B, N, K)
+        # ind = self.indicator_predictor(z_H.mean(dim=1, keepdim=True)) # (B, 1, K)
+        # im = torch.cat([inc, ind], dim=1) # (B, N+1, K)
+        # im = im.transpose(1, 2)
+        inc = (z_H @ torch.transpose(z_L, 1, 2)) / math.sqrt(self.hidden_dim) # (B, K, N)
+        ind = self.indicator_predictor(z_H) # (B, K, 1)
+        im = torch.cat([inc, ind], dim=2) # (B, K, N+1)
 
         ### new state
         state = HiddenState(z_L=z_L.detach(), z_H=z_H.detach())
