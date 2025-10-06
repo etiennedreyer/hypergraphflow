7ef2c275f8b2d78a2ca24ae9c2351694b8d8b659
Nilotpal Kakati <nilotpal.kakati@cern.ch>
2025-10-06 10:43:35 +0300

    attention update; wip

--- models/transformer.py
+++ models/transformer.py
@@ -23,12 +23,28 @@ class ContextProjector(nn.Module):
         return affine_params
 
 
+class LayerScale(nn.Module):
+    """Applies the LayerScale operation from the Cait vision transformer.
+
+    Effective at improving stability and speed of deep transformers.
+    Now the standard for vision transformers
+    https://arxiv.org/abs/2103.17239
+    """
+
+    def __init__(self, dim: int, init_value: float = 1e-3) -> None:
+        super().__init__()
+        self.gamma = nn.Parameter(init_value * torch.ones(dim))
+
+    def forward(self, x):
+        return x * self.gamma
+
+
 def modulate(x, shift, scale):
     return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
 
 
 class DiTLayer(nn.Module):
-    def __init__(self, embed_dim, context_dim, mha_config, dense_config=None):
+    def __init__(self, embed_dim, context_dim, mha_config, dense_config=None, layer_scale=False):
         super().__init__()
         self.embed_dim = embed_dim
         self.mha = MultiheadAttentionVarLen(embed_dim, **mha_config)
@@ -43,6 +59,10 @@ class DiTLayer(nn.Module):
 
         self.adaLN_modulation = nn.Sequential(
             nn.SiLU(), nn.Linear(context_dim, 6 * embed_dim, bias=True))
+
+        if layer_scale:
+            self.layer_scale_attn = LayerScale(embed_dim)
+            self.layer_scale_mlp = LayerScale(embed_dim)
 
     def forward(self, q, q_mask=None, kv=None, kv_mask=None, context=None, attn_mask=None, attn_bias=None):
         '''
@@ -60,11 +80,15 @@ class DiTLayer(nn.Module):
             q_attn = self.mha(
                 q=q, kv=modulate(self.norm1(kv), shift_msa, scale_msa),
                 q_mask=q_mask, kv_mask=kv_mask, attn_mask=attn_mask, attn_bias=attn_bias)
-            
+    
+        if hasattr(self, 'layer_scale_attn'):
+            q_attn = self.layer_scale_attn(q_attn)
         q = q + gate_msa.unsqueeze(1) * q_attn
         
         if self.dense:
             q_mlp = self.dense(modulate(self.norm2(q), shift_mlp, scale_mlp), context)
+            if hasattr(self, 'layer_scale_mlp'):
+                q_mlp = self.layer_scale_mlp(q_mlp)
             q = q + gate_mlp.unsqueeze(1) * q_mlp
 
         return q
